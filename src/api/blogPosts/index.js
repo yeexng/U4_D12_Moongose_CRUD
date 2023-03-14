16952393c3f382593c79bbd56bd98f677e544013
yeexng <118996751+yeexng@users.noreply.github.com>
2023-03-14 15:10:48 +0000

    CRUD method completed

--- src/api/blogPosts/index.js
+++ src/api/blogPosts/index.js
@@ -16,27 +16,68 @@ blogPostsRouter.post("/", async (req, res, next) => {
 
 blogPostsRouter.get("/", async (req, res, next) => {
   try {
+    const blogPosts = await BlogPostsModel.find();
+    res.send(blogPosts);
   } catch (error) {
     next(error);
   }
 });
 
-blogPostsRouter.get("/blogPostsID", async (req, res, next) => {
+blogPostsRouter.get("/:blogPostsID", async (req, res, next) => {
   try {
+    const blogPost = await BlogPostsModel.findById(req.params.blogPostsID);
+    if (blogPost) {
+      res.send(blogPost);
+    } else {
+      next(
+        createHttpError(
+          404,
+          `BlogPost with id ${req.params.blogPostsID} not found!`
+        )
+      );
+    }
   } catch (error) {
     next(error);
   }
 });
 
-blogPostsRouter.put("/blogPostsID", async (req, res, next) => {
+blogPostsRouter.put("/:blogPostsID", async (req, res, next) => {
   try {
+    const updatedBlogPost = await BlogPostsModel.findByIdAndUpdate(
+      req.params.blogPostsID,
+      req.body,
+      { new: true, runValidators: true }
+    );
+    if (updatedBlogPost) {
+      res.send(updatedBlogPost);
+    } else {
+      next(
+        createHttpError(
+          404,
+          `BlogPost with id ${req.params.blogPostsID} not found!`
+        )
+      );
+    }
   } catch (error) {
     next(error);
   }
 });
 
-blogPostsRouter.delete("/blogPostsID", async (req, res, next) => {
+blogPostsRouter.delete("/:blogPostsID", async (req, res, next) => {
   try {
+    const deletedBlogPost = await BlogPostsModel.findByIdAndDelete(
+      req.params.blogPostsID
+    );
+    if (deletedBlogPost) {
+      res.status(204).send();
+    } else {
+      next(
+        createHttpError(
+          404,
+          `BlogPost with id ${req.params.blogPostsID} not found!`
+        )
+      );
+    }
   } catch (error) {
     next(error);
   }
